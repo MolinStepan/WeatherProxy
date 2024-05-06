046d7d4df077701064e766b9084c038534716f35
Molin Stepan <molin.stepan@gmail.com>
2024-05-06 16:16:03 +0300

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -14,7 +14,7 @@ import           Weather
 -- http://api.openweathermap.org/geo/1.0/direct?q={City}&limit={num}&appid={API key}
 -- https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-apiKey = "" -- test api key hidden from git
+apiKey = "d3893a4a4a6a642eadad642c112ed456" -- test api key hidden from git
 
 second :: Int
 second = 1_000_000
@@ -29,7 +29,7 @@ main :: IO ()
 main = do
   manager' <- newManager defaultManagerSettings
   res <- runClientM
-    (cityRequest (Just "Moscow") (Just 1) (Just ""))
+    (cityRequest (Just "Moscow") (Just 1) (Just apiKey))
     (mkClientEnv manager' (BaseUrl Http "api.openweathermap.org" 80 ""))
   case res of
     Left err    -> putStrLn $ "Error: " ++ show err
